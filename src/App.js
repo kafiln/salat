8abc4c6a365620e59d6d9e2a96853698d26837a0
Kafil Nasdami <kafil.nasdami@gmail.com>
2019-03-16 22:11:43 +0100

    Initial commit-

--- src/App.js
+++ src/App.js
@@ -1,28 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import cities from './data/fr/cities_fr.json'
 import './App.css';
+import Autocomplete from 'react-autocomplete';
+import getPrayers from './getPrayers'
 
-class App extends Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      id: '',
+      value: '',
+      result: null
+    }
+  }
+
+  shouldItemRender = (item, value) => item.name.toLowerCase().indexOf(value.toLowerCase()) > -1;
+  getItemValue = city => {
+    console.log("getting!")
+    return city.name;
+  }
+  onSelect = value => {
+    this.setState({ value: value })
+    const id = cities.filter(e => e.name === value)[0].id;
+    getPrayers(id);
+  }
+
+  onChange = e => {
+    console.log("changing!")
+    this.setState({ value: e.target.value })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Please choose your city</h1>
+          <Autocomplete
+            getItemValue={this.getItemValue}
+            items={cities}
+            key={city => city.id}
+            shouldItemRender={this.shouldItemRender}
+            renderItem={(city, isHighlighted) =>
+              <div key={city.id} style={{ background: isHighlighted ? '#eee' : 'transparent', borderRadius: '20' }}>
+                {city.name}
+              </div>
+            }
+            value={this.state.value}
+            onChange={this.onChange}
+            onSelect={this.onSelect}
+          />
+
         </header>
+        {this.state.result && <table>
+          {this.result.length}
+        </table>}
       </div>
     );
   }
 }
 
-export default App;
