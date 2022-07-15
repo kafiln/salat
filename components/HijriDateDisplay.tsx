dc65d8c4d82b33e008f4861c8f0b2d670751f9fd
Kafil <kafil.nasdami@gmail.com>
2022-07-16 00:25:17 +0100

    update font size for hijri date

--- components/HijriDateDisplay.tsx
+++ components/HijriDateDisplay.tsx
@@ -2,7 +2,7 @@ import { Text } from "@chakra-ui/react";
 import React from "react";
 
 const HijriDateDisplay = ({ date }: { date: string }) => {
-  return <Text fontSize="xl">{date}</Text>;
+  return <Text>{date}</Text>;
 };
 
 export default React.memo(HijriDateDisplay);
