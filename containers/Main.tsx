87d0565b39d2b2abb2766bded1d564e474a661b1
Kafil NASDAMI <nasdami.kafil@bcg.com>
2023-02-04 00:04:25 +0100

    only display daily times

--- containers/Main.tsx
+++ containers/Main.tsx
@@ -1,20 +1,17 @@
 import { Flex, VStack } from "@chakra-ui/react";
 import HijriDateDisplay from "@components/HijriDateDisplay";
-import Periodicity from "@components/Periodicity";
 import PrayerFooter from "@components/Prayer/PrayerFooter";
 import SelectCity from "@components/SelectCity";
 import { getHijriDate } from "api/prayers";
-import { setGlobalCity, togglePeriodicity, UseAppContext } from "context";
+import { setGlobalCity, UseAppContext } from "context";
 import { getAllCities, getCityName } from "data/cityService";
 import { useQuery } from "react-query";
 import Daily from "./Daily";
-import Monthly from "./Monthly";
 
 const Main = () => {
   const [state, dispatch] = UseAppContext();
   const cities = getAllCities();
-  const { city, periodicity } = state;
-  const isDaily = periodicity === "DAILY";
+  const { city } = state;
   const { data: hijri } = useQuery(["hijri"], () => getHijriDate());
 
   const handleChange = (city: number) => {
@@ -26,14 +23,10 @@ const Main = () => {
   return (
     <Flex height="100%" direction="column">
       <VStack flexDir="column" spacing={4}>
-        <Periodicity
-          isDaily={isDaily}
-          onClick={() => dispatch(togglePeriodicity())}
-        />
         <SelectCity cities={cities} city={city} handleChange={handleChange} />
         <HijriDateDisplay date={hijri} />
       </VStack>
-      {isDaily ? <Daily /> : <Monthly />}
+      <Daily />
       <PrayerFooter city={getCityName(city)} />
     </Flex>
   );
