8dc0777d7265a416673972856d724718161ffff9
Kafil Nasdami <kafil.nasdami@gmail..com>
2023-12-28 00:03:53 +0100

    no need for axios

--- client/prayers.ts
+++ client/prayers.ts
@@ -1,27 +1,19 @@
-import axios from "axios";
 import dayjs from "dayjs";
 
 const MONTHLY_URL = "/api/monthly/";
 const DAILY_URL = "/api/daily/";
 
-export const getHijriDate = async () => {
-  return await axios
-    .get(`https://apisearch.hadithm6.com/api/hijridate`)
-    .then((res) => res.data).catch(err => console.error("Hijri date is not available at the moment", err))
-};
 
 export const getPrayers = async (city: number) => {
-
   const data = await fetch(`${DAILY_URL}${city}`).then((res) =>
     res.json()
   );
 
-  return data.map((item: any) => ({...item,time:dayjs(item.time)}))
+  return data.map((item: any) => ({ ...item, time: dayjs(item.time) }))
 };
 
 export const getMonthlyPrayers = async (city: number) => {
-  const { data } = await fetch(`${MONTHLY_URL}${city}`).then((res) =>
+  return await fetch(`${MONTHLY_URL}${city}`).then((res) =>
     res.json()
   );
-  return data;
 };
--- pages/api/monthly/[id].ts
+++ pages/api/monthly/[id].ts
@@ -5,7 +5,5 @@ import { getMonthlyPrayers } from 'utils';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const { id } = req.query;
     const data = await getMonthlyPrayers(Number(id));
-    return res.json({
-        data,
-    });
+    return res.json(data);
 }
