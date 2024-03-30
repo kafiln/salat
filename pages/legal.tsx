7b46923aeefb5d3f05847592eba94dab20acf726
Kafil Nasdami <kafil.nasdami@gmail..com>
2024-03-30 01:06:34 +0000

    added a legal page

---
+++ pages/legal.tsx
@@ -0,0 +1,69 @@
+function legal() {
+  return (
+    <>
+      <h1>Mentions légales</h1>
+      <h2>Accès et utilisation</h2>
+      <h3>L’accès au site de la Salaty Maroc est possible 24h/24 et 7j/7. </h3>
+
+      <p>
+        Toute tentative d’entraver ou de fausser le fonctionnement de notre
+        système d’information est passible de sanctions pénales.
+      </p>
+
+      <h3>Propriété intellectuelle du contenu</h3>
+      <p>
+        Sauf mention contraire, la Salaty Maroc est titulaire des droits
+        d'auteur sur tout le contenu figurant sur le site, notamment les textes,
+        les images, les vidéos, le graphisme les logos et tout autre élément qui
+        le compose.
+      </p>
+
+      <p>
+        Toute utilisation non autorisée du contenu du site, notamment à des fins
+        commerciales est interdite et pourra faire l’objet de poursuites
+        juridiques.
+      </p>
+
+      <h3>Caractère des informations présentes sur le site</h3>
+      <p>
+        Les informations présentes sur ce site web n'ont qu'un caractère
+        indicatif. Ni leur contenu ni leur exactitude ne peuvent être garantis.
+        Ces informations n'engagent pas contractuellement la Salaty Maroc.
+      </p>
+
+      <p>
+        La Salaty Maroc décline toute responsabilité sur les décisions qui
+        pourraient être prises à partir de ces informations.
+      </p>
+
+      <h3>Protection des données personnelles</h3>
+      <p>
+        Chaque utilisateur de ce site peut être sollicité par la Salaty Maroc à
+        lui communiquer des informations personnelles, telles que nom, prénom,
+        adresse électronique.
+      </p>
+
+      <p>
+        Ces informations sont recueillies conformément à la loi 09-08 relative à
+        la protection des personnes physiques à l’égard du traitement des
+        données à caractère personnel, et sont nécessaires pour vous identifier
+        afin de vous permettre l’accès aux services offerts en ligne.
+      </p>
+
+      <p>
+        Susceptibles de faire l’objet de traitements automatisés, lesdites
+        informations sont destinées au seul usage de la Salaty Maroc, et ne
+        seront en aucun cas ni utilisées à des fins autres que celles pour
+        lesquelles elles ont été collectées, ni mises à la disposition de tiers.
+      </p>
+
+      <p>
+        Vos informations à caractère personnel sont protégées en vertu des
+        dispositions de la loi susmentionnée, laquelle loi vous octroie le droit
+        à l’information, le droit d’accès à vos données personnelles, le droit
+        de rectification et enfin le droit d’opposition.
+      </p>
+    </>
+  );
+}
+export default legal;
