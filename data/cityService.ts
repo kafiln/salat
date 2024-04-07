450e70d03530f3ca252daefdced512a16cf6bbfb
Kafil Nasdami <kafil.nasdami@gmail..com>
2024-04-07 03:09:29 +0000

    add gps coordinates to city api

--- data/cityService.ts
+++ data/cityService.ts
@@ -1,15 +1,34 @@
 import cities from "data/cities.json";
 
+export type City = {
+  id: number;
+  name: string;
+  long_d: number;
+  long_m: number;
+  lat_d: number;
+  lat_m: number;
+  alt: number;
+};
+
 export const getCityName = (city: number) => {
   const cityName = cities.find((c) => c.id === city);
   return cityName ? cityName.name : "";
 };
 
-export const getAllCities = () => {
-  return cities;
+const cityMapper = (city: City) => {
+  return {
+    id: city.id,
+    name: city.name,
+    longitude: Number(`${city.long_d}.${city.long_m}`),
+    latitude: Number(`${city.lat_d}.${city.lat_m}`),
+    elevation: city.alt,
+  };
 };
 
+export const getAllCities = () => {
+  return cities.map(cityMapper);
+};
 
 export const getCityById = (id: number) => {
-  return cities.find(i => i.id === id)
+  return cityMapper(cities.find((i) => i.id === id)!);
 };
